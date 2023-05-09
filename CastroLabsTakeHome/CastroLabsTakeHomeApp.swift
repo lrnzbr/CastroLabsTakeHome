7bce673e83eaf6d91911e580b0783bbf6e6cf297
Lorenzo Brown <lrnzbr@gmail.com>
2023-05-09 17:14:33 -0600

    Initial Commit

---
+++ CastroLabsTakeHome/CastroLabsTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastroLabsTakeHomeApp.swift
+//  CastroLabsTakeHome
+//
+//  Created by Lorenzo Brown on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CastroLabsTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
